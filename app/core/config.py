f9595c1029b1730549703e6856b59866f287bbdf
Yikang Li (Ethan) <ethanli2003@outlook.com>
2025-09-23 04:36:41 +1000

    config firebase

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str = "FastAPI App"
     MONGO_DSN: str = "mongodb://localhost:27017/fastapi_db"
+    FIREBASE_CRED_PATH: str | None = None  # None values will be covered by values in .env
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
