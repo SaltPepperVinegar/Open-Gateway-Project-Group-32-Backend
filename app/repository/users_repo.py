73df3c1f79c0f24560953d02e25c7da57f912a74
Alex Wang <Alex226336@gmail.com>
2025-09-22 16:42:48 +1000

    added user create

--- app/repository/users_repo.py
+++ app/repository/users_repo.py
@@ -1,8 +1,17 @@
+from app.models.base.user import UserCreate
 from app.models.db.user import UserDoc
 
 
-async def create_user(userdoc: UserDoc) -> UserDoc:
-    return await userdoc.insert()
+async def create_user(user_create: UserCreate) -> UserCreate:
+    userdoc = UserDoc(
+        username=user_create.username,
+        email=user_create.email,
+        role=user_create.role,
+        password_hash=user_create.hashed,
+    )
+    user_create.userID = str(userdoc.id)
+    await userdoc.insert()
+    return user_create
 
 
 async def get_user_by_username(username: str) -> UserDoc | None:
