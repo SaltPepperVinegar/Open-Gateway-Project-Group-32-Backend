97cc89575edb5223386b36cffce1be6caf219236
Alex Wang <Alex226336@gmail.com>
2025-10-12 01:12:13 +1100

    added tile area remove

--- app/service/tile_area_update_service.py
+++ app/service/tile_area_update_service.py
@@ -1,5 +1,5 @@
 from app.models.DTO.tile import TileAreaRemoveDTO, TileAreaUpdateDTO
-from app.repository.tile.tiling_area import update_tile_area
+from app.repository.tile.tiling_area import tile_area_set_inactive, update_tile_area
 from app.repository.tile.tiling_job_create import create_tiling_job
 
 
@@ -10,4 +10,4 @@ async def update_tile_area_service(update: TileAreaUpdateDTO):
 
 
 async def remove_tile_area(remove: TileAreaRemoveDTO):
-    pass
+    tile_area_set_inactive(remove)
