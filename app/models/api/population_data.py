517b4797d60f78ab60169720d4f4b13075e167db
Alex Wang <Alex226336@gmail.com>
2025-10-13 23:09:15 +1100

    updated input output of population data api

--- app/models/api/population_data.py
+++ app/models/api/population_data.py
@@ -1,18 +1,13 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from app.models.general.geo_json import GeoJSONPolygon
 
 
 class PopulationWindowIn(BaseModel):
     # [min_lon, min_lat, max_lon, max_lat]
-    bbox: list[float] = Field(..., min_items=4, max_items=4)
+    area: GeoJSONPolygon
     time_from: Optional[datetime] = None
     time_to: Optional[datetime] = None
-
-
-class AreaPopulationOut(BaseModel):
-    area: GeoJSONPolygon
-    population: float
