755aaaab65e308cc535bacd971d7028077c2d895
Yikang Li (Ethan) <ethanli2003@outlook.com>
2025-10-20 22:36:11 +1100

    Update disaster_area_service.py

--- app/service/disaster_area_service.py
+++ app/service/disaster_area_service.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, List
 
+from beanie import PydanticObjectId
 from fastapi import HTTPException, status
 
 from app.exceptions.disaster_area import WorkerCreatesDisasterAreaError
@@ -13,11 +14,17 @@ from app.models.api.disaster_area import (
     DisasterAreaUpdateRes,
 )
 from app.models.DTO.disaster_area import DisasterAreaCreateDTO, DisasterAreaUpdateDTO
-from app.models.embedded.enums import UserRole
+from app.models.DTO.tile import TileAreaRemoveDTO, TileAreaUpdateDTO
+from app.models.embedded.enums import DisasterAreaStatus, UserRole
+from app.models.general.geo_json import GeoJSONPolygon as GeoJSONPolygonTile
 from app.repository.disaster_area_repo import (
     create_disaster_area,
     search_disaster_area,
     update_disaster_area,
+)
+from app.service.tile_area_update_service import (
+    remove_tile_area,
+    update_tile_area_service,
 )
 
 
@@ -42,6 +49,18 @@ async def create_disaster_area_service(
 
     created_disaster_area = await create_disaster_area(disaster_area_dto)
 
+    # Create tile update job for the new disaster area
+
+    await update_tile_area_service(
+        TileAreaUpdateDTO(
+            area=GeoJSONPolygonTile(
+                type=created_disaster_area.boundary.type,
+                coordinates=created_disaster_area.boundary.coordinates,
+            ),
+            area_id=PydanticObjectId(created_disaster_area.id),
+        )
+    )
+
     return DisasterAreaCreateRes(**created_disaster_area.model_dump())
 
 
@@ -62,5 +81,27 @@ async def update_disaster_area_service(
         raise InvalidObjectIDStringError() from ValueError
 
     disaster_area_dto = await update_disaster_area(update_dto)
+
+    # Update tile job according to status
+
+    if (
+        update_info.status == DisasterAreaStatus.RESOLVED
+        or update_info.status == DisasterAreaStatus.DELETED
+    ):
+        # If new status is resolved or deleted, remove corresponding tile job
+        await remove_tile_area(
+            TileAreaRemoveDTO(area_id=PydanticObjectId(disaster_area_dto.id))
+        )
+    else:
+        # If new status is active, create tile job
+        await update_tile_area_service(
+            TileAreaUpdateDTO(
+                area=GeoJSONPolygonTile(
+                    type=disaster_area_dto.boundary.type,
+                    coordinates=disaster_area_dto.boundary.coordinates,
+                ),
+                area_id=PydanticObjectId(disaster_area_dto.id),
+            )
+        )
 
     return DisasterAreaUpdateRes(**disaster_area_dto.model_dump())
